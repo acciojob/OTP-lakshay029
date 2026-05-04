c8aff4db88ba6bf1aeda18277752a650e4e50d3e
lakshay <126890120+lakshay029@users.noreply.github.com>
2026-05-04 11:54:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const inputs=document.querySelectorAll(".code")
+
+inputs.forEach((input,i)=>{
+	input.addEventListener('keyup',(e)=>{
+		if(e.key>='0' && e.key<='9'){
+			inputs[i+1].focus();
+		}else if(e.key=='Backspace'){
+			
+			inputs[i-1].focus();
+		}
+		else{
+			e.preventDefault();
+		}
+	})
+})
